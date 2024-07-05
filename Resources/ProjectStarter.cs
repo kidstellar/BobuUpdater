aa995a2cc0ed9451382e0468b04c17bb4bd1a600
Arda Karaman <145969396+ArdaKaramanK@users.noreply.github.com>
2024-07-05 06:17:51 +0300

    Add files via upload

---
+++ Resources/ProjectStarter.cs
@@ -0,0 +1,115 @@
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Net;
+
+public class ProjectStarter : Editor
+{
+    //Starter
+    private static string[] githubUrls = new string[]
+    {
+        "https://github.com/kidstellar/BobuPackages/raw/main/Dotween.unitypackage", // Buraya GitHub URL'lerini ekleyin.
+        "https://github.com/kidstellar/BobuPackages/raw/main/BobuEditor.unitypackage"
+    };
+
+    private static bool isImporting = false;
+
+    //Güncelleme
+    private const string packageUrl = "https://github.com/kidstellar/BobuPackages/raw/main/BobuEditor.unitypackage";
+
+    //Projeyi çalışma kurulumunu yapar
+    [MenuItem("Bobu/Start Project", true)]
+    public static bool ValidateStartProject()
+    {
+        // isImporting true ise menü öğesini devre dışı bırak
+        return !isImporting;
+    }
+
+    [MenuItem("Bobu/Start Project")]
+    public static void StartProject()
+    {
+        if (isImporting)
+        {
+            // İşlem tamamlandı, kullanıcıya bilgi ver
+            EditorUtility.DisplayDialog("Project Start", "Project start process completed.", "OK");
+            Debug.LogWarning("Import process is already in progress.");
+            return;
+        }
+
+        isImporting = true;
+
+        foreach (var url in githubUrls)
+        {
+            string fileName = Path.GetFileName(url);
+            string downloadPath = Path.Combine("Temp", fileName);
+            DownloadAndImportPackage(url, downloadPath);
+        }
+
+    }
+
+    private static void DownloadAndImportPackage(string url, string path)
+    {
+        using (WebClient client = new WebClient())
+        {
+            try
+            {
+                // Dosyayı indir
+                client.DownloadFile(url, path);
+                Debug.Log("Package downloaded successfully: " + path);
+
+                // İndirilen dosyayı import et
+                ImportPackage(path);
+            }
+            catch (WebException ex)
+            {
+                Debug.LogError("Failed to download the package: " + ex.Message);
+            }
+        }
+    }
+
+    private static void ImportPackage(string path)
+    {
+        if (File.Exists(path))
+        {
+            AssetDatabase.ImportPackage(path, false);
+            Debug.Log("Package imported successfully: " + path);
+
+            // İndirme ve import işlemi tamamlandıktan sonra geçici dosyayı sil
+            File.Delete(path);
+        }
+        else
+        {
+            Debug.LogError("Package file not found at path: " + path);
+        }
+    }
+
+    //Bobu Editörün güncel olup olmadığını kontrol eder
+    [MenuItem("Bobu/Update Editor")]
+    public static void UpdatePackage()
+    {
+        DownloadAndInstallPackage();
+    }
+
+    private static void DownloadAndInstallPackage()
+    {
+        string tempDownloadPath = Path.Combine(Application.temporaryCachePath, "BobuEditor.unitypackage");
+
+        using (WebClient client = new WebClient())
+        {
+            try
+            {
+                client.DownloadFile(packageUrl, tempDownloadPath);
+                Debug.Log("Package downloaded successfully.");
+
+                AssetDatabase.ImportPackage(tempDownloadPath, false);
+                Debug.Log("Package imported successfully.");
+
+                File.Delete(tempDownloadPath);
+            }
+            catch (WebException ex)
+            {
+                Debug.LogError("Failed to download the package: " + ex.Message);
+            }
+        }
+    }
+}
