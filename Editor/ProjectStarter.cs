1b2efea65472e4575dacaaf748f68e8b470a8489
Arda Karaman <145969396+ArdaKaramanK@users.noreply.github.com>
2024-07-17 10:52:53 +0300

    V1.0.4

--- Editor/ProjectStarter.cs
+++ Editor/ProjectStarter.cs
@@ -16,6 +16,7 @@ public class ProjectStarter : Editor
 
     //Güncelleme
     private const string packageUrl = "https://github.com/kidstellar/BobuPackages/raw/main/BobuEditor.unitypackage";
+    private const string ImageBundleUrl = "https://github.com/kidstellar/BobuPackages/raw/main/ImageBundleEditor.unitypackage";
 
     // Projeyi çalışma kurulumunu yapar
     [MenuItem("Bobu/Updater/Start Project", true)]
@@ -116,6 +117,42 @@ public class ProjectStarter : Editor
             {
                 // Paketi indir
                 client.DownloadFile(packageUrl, tempDownloadPath);
+                Debug.Log("Package downloaded successfully.");
+
+                // Dizinleri sil
+                DeleteDirectory("Resources/EditorScript");
+                DeleteDirectory("Resources/Mechanics");
+
+                // Paketi içe aktar
+                AssetDatabase.ImportPackage(tempDownloadPath, false);
+                Debug.Log("Package imported successfully.");
+
+                // Geçici dosyayı sil
+                File.Delete(tempDownloadPath);
+            }
+            catch (WebException ex)
+            {
+                Debug.LogError("Failed to download the package: " + ex.Message);
+            }
+        }
+    }
+
+    [MenuItem("Bobu/Updater/Image Bundle Builder/Image Builder")]
+    public static void UpdatePackageImage()
+    {
+        DownloadAndInstallPackageImage();
+    }
+
+    private static void DownloadAndInstallPackageImage()
+    {
+        string tempDownloadPath = Path.Combine(Application.temporaryCachePath, "ImageBundleEditor.unitypackage");
+
+        using (WebClient client = new WebClient())
+        {
+            try
+            {
+                // Paketi indir
+                client.DownloadFile(ImageBundleUrl, tempDownloadPath);
                 Debug.Log("Package downloaded successfully.");
 
                 // Dizinleri sil
